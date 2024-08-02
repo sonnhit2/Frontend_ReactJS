476e1ef55e3889862360dd3d91e0ac3dbbc78623
Nguyễn Hồng Sơn <sonnh.it2@vnpt.vn>
2024-08-02 10:59:18 +0700

    hoan tat tao giao dien form login

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -1,153 +1,43 @@
 import React, {Component} from "react";
 import { connect } from "react-redux";
 import {push} from 'connected-react-router';
-
 import * as actions from "../../store/actions";
-import { KeyCodeUtils, LanguageUtils } from "../../utils";
-
-import userIcon from '../../assets/images/user.svg';
-import passIcon from '../../assets/images/pass.svg';
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
 
-import adminService from "../../services/adminService";
 
 class Login extends Component{
     constructor(props){
         super(props);
-        this.btnLogin = React.createRef();
     }
 
-    initialState = {
-        username:'',
-        password:'',
-        loginError:''
-    }
-
-    state = {
-        ...this.initialState
-    }
-
-    refresh = () =>{
-        this.setState({
-            ...this.initialState
-        })
-    }
-
-    onUsernameChange=(e)=>{
-        this.setState({ username: e.target.value })
-    }
-
-    onPasswordChange = (e) => {
-        this.setState({ password: e.target.value })
-    }
-
-    redirectToSystemPage = () => {
-        const { navigate } = this.props;
-        const redirectPath = '/system/user-manage';
-        navigate('${redirectPath}');
-    }
-
-    processLogin = () => {
-        const { username, password } = this.state;
-
-        const { adminLoginSuccess, adminLoginFail } = this.props;
-        let loginBody = {
-            username: 'admin',
-            password: '123456'
-        }
-        //sucess
-        let adminInfo = {
-            "tlid": "0",
-            "tlfullname": "Administrator",
-            "custype": "A",
-            "accessToken": "eyJhbGciOiJIU" 
-        }
-
-        adminLoginSuccess(adminInfo);
-        this.refresh();
-        this.redirectToSystemPage();
-        try {
-            adminService.login(loginBody)
-        } catch (e) {
-            console.log('error login: ', e)
-        }
-
-    }
-
-    handlerKeyDown = (event) => {
-        const keyCode = event.which || event.keyCode;
-        if(keyCode === KeyCodeUtils.ENTER){
-            event.preventDefault();
-            if(!this.btnLogin.current || this.btnLogin.current.disabled) return;
-            this.btnLogin.current.click();
-        }
-    };
-
-    componentDidMount(){
-        document.addEventListener('keydown', this.handlerKeyDown);
-    }   
-    //reactjs life-cycle method in Unmounting phrase, called when the component is about to be removed fom DOM
-    componentWillUnmount(){
-        document.removeEventListener('keydown', this.handlerKeyDown);
-        // fix Warning: Can't perform a React state update on an unmouted component
-        this.setState = (state, callback) => {
-            return;
-        }
-    }
     //reactjs life-cycle method. The render() method is required, and is the method that actually outputs the HTML to the DOM.
     render(){
-        const { username, password, loginError } = this.state;
-        const { lang } = this.props;
-
         return (
-            <div className="login-wrapper">
+            <div className="login-background">
                 <div className="login-container">
-                    <div className="form_login">
-                        <h2 className="title">
-                            <FormattedMessage id="login.login" />
-                        </h2>
-                        <div className="form-group icon-true">
-                            <img className="icon" src={userIcon} alt="this" />
-                            <input 
-                                placeholder={LanguageUtils.getMessageByKey("login.username",lang)}
-                                id="username"
-                                name="username"
-                                type="text"
-                                className="form-control"
-                                value={username}
-                                onChange={this.onUsernameChange}
-                            />
+                    <div className="login-content row">
+                        <div className="col-12 text-login">Login</div>
+                        <div className="col-12 form-group login-input">
+                            <label>Username:</label>
+                            <input type="text" className="form-control" placeholder="Enter your username" />
                         </div>
-                        
-                        <div id="phone-input-container" className="form-group icon-true">
-                            <img className="icon" src={passIcon} alt="this" />
-                            <input  
-                                placeholder={LanguageUtils.getMessageByKey("login.password", lang)}
-                                id="password" 
-                                name="password" 
-                                type="password" 
-                                className="form-control"
-                                value={password} 
-                                onChange={this.onPasswordChange}
-                            />
+                        <div className="col-12 form-group login-input">
+                            <label>Password:</label>
+                            <input type="password" className="form-control" placeholder="Enter your password" />
                         </div>
-
-                        {loginError !== '' && (
-                            <div className='login-error'>
-                                <span className='login-error-message'>{loginError}</span>
-                            </div>
-                        )}
-
-                        <div className="form-group login">
-                            <input 
-                                ref={this.btnLogin}
-                                id="btnLogin"
-                                type="submit"
-                                className="btn"
-                                value={LanguageUtils.getMessageByKey("login.login", lang)}
-                                onClick={this.processLogin}
-                            />
+                        <div className="col-12">
+                            <button className="btn-login">Login</button>
+                        </div>                        
+                        <div className="col-12">
+                            <span className="forgot-password">Forgot your password?</span>
+                        </div>
+                        <div className="col-12 text-center mt-3">
+                            <span className="text-other-login">Or Login with:</span>
+                        </div>
+                        <div className="col-12 social-login">
+                            <i className="fab fa-google-plus-g google"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
                         </div>
                     </div>
                 </div>
@@ -158,7 +48,7 @@ class Login extends Component{
 
 const mapStateToProps = state => {
     return {
-        lang: state.app.language
+        language: state.app.language
     };
 };
 
