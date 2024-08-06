133d6503b40143f23662a9841c0a6fcfe755188a
Nguyễn Hồng Sơn <sonnh.it2@vnpt.vn>
2024-08-06 20:36:20 +0700

    hoàn thiện chức năng liet ke danh sach user

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,8 +5,10 @@ const userService = {
     handleLoginApi: (email, password) => {
         return axios.post('/api/login',{email, password});
     },
+    getAllUsers: (inputId) =>{
+        return axios.get(`/api/get-all-users?id=${inputId}`);
+    }
 }
-
 
 
 export default userService;
